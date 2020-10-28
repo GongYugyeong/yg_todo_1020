ea2f4f5d5f64ad733af90d0a1033387ea386ecd7
GongYugyeong <yugyeong0917@naver.com>
2020-10-28 16:01:11 +0900

    첫번째 Todo React를 jsx로 변환하기

--- todo.js
+++ todo.js
@@ -1,59 +1,28 @@
-const title = React.createElement(
-  'h1',
-  {className:"text-center"},
-  'Todo List'
+const title = <h1 className='text-center'>Todo List</h1>
+
+const form = (
+  <form className="text-center mb-3">
+    <input type="text" className="form-control mb-3" />
+    <button className="btn btn-primary">Add Todo</button>
+  </form>
 );
 
-const input = React.createElement(
-  'input',
-  {type:'text', className:'form-control mb-3'}
+const list = (
+  <ul className="list-group">
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      <input type="checkbox" />
+      <span className="flex-grow-1 ml-3">영어 공부하기</span>
+      <button className="btn btn-danger">Delete</button>
+    </li>
+  </ul>
 );
 
-const button = React.createElement(
-  'button',
-  {className:'btn btn-primary'},
-  'Add Todo'
+const container = (
+  <div className='container mt-3'>
+    {title}
+    {form}
+    {list}
+  </div>
 );
 
-const form = React.createElement(
-  'form',
-  {className:'text-center mb-3'},
-  input,button
-);
-
-const li_input = React.createElement(
-  'input',
-  {type:'checkbox'}
-);
-
-const li_span = React.createElement(
-  'span',
-  {className:'flex-grow-1 ml-3'},
-  '영어 공부하기'
-);
-
-const li_button = React.createElement(
-  'button',
-  {className:'btn btn-danger'},
-  'Delete'
-);
-
-const li = React.createElement(
-  'li',
-  {className:'list-group-item d-flex justify-content-between align-items-center'},
-  li_input, li_span, li_button
-);
-
-const ul = React.createElement(
-  'ul',
-  {className:'list-group'},
-  li
-);
-
-const Todo = React.createElement(
-  'div',
-  {className:'container mt-3'},
-  title,form,ul
-);
-
-ReactDOM.render(Todo,document.getElementById('todo'));
+ReactDOM.render(container,document.getElementById('todo'));
